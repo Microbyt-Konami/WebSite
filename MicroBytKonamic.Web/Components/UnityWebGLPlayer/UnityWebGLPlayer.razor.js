9504d6e289ced1763795a82355774ccccf21bba1
bugbit <ohernandezba@gmail.com>
2024-01-13 10:14:55 +0100

    ver postal

---
+++ MicroBytKonamic.Web/Components/UnityWebGLPlayer/UnityWebGLPlayer.razor.js
@@ -0,0 +1,12 @@
+﻿export function onLoad() {
+    console.log('Loaded');
+    alert("Helloworld")
+}
+
+export function onUpdate() {
+    console.log('Updated');
+}
+
+export function onDispose() {
+    console.log('Disposed');
+}
