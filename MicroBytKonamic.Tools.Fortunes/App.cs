b1b231100d6f285ecff2203bf72640cfd66b8579
bugbit <ohernandezba@gmail.com>
2024-01-31 20:20:15 +0100

    init tools fortunes

---
+++ MicroBytKonamic.Tools.Fortunes/App.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroBytKonamic.Tools.Fortunes
+{
+    internal class App
+    {
+        private readonly IPostalesServices _services;
+
+        public App(IPostalesServices services)
+        {
+            _services = services;
+        }
+
+        public async Task RunAsync(string[] args)
+        {
+            var postales = await  _services.GetFelicitacionAsync(new GetFelicitacionIn { Anyo = 2023, Intervals = new IntegerIntervals() });
+
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ MicroBytKonamic.Tools.Fortunes/Program.cs
@@ -0,0 +1,43 @@
+﻿// https://andrewlock.net/using-dependency-injection-in-a-net-core-console-application/
+
+using MicroBytKonamic.Tools.Fortunes;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder
+(
+    new HostApplicationBuilderSettings
+    {
+        Args = args,
+#if DEBUG
+        EnvironmentName = Environments.Development
+#else
+        EnvironmentName = Environments.Production
+#endif
+    }
+    );
+
+builder.Services.AddLocalization();
+builder.Services.AddSingleton<IResourcesServices, ResourcesServices>();
+builder.Services.AddMicrobytKonamic();
+builder.Services.AddScoped<App>();
+
+using IHost host = builder.Build();
+
+var scope = host.Services.CreateScope();
+
+var conf = scope.ServiceProvider.GetRequiredService<IConfiguration>();
+var hostenvv = scope.ServiceProvider.GetRequiredService<IHostEnvironment>();
+
+string? x = conf.GetValue<string>("a");
+
+try
+{
+    await scope.ServiceProvider.GetRequiredService<App>().RunAsync(args);
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
