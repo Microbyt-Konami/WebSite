0bed0ed6ceeeba23642a160f3c274f7b652b94b8
bugbit <ohernandezba@gmail.com>
2024-02-23 14:03:40 +0100

    about page

--- MicroBytKonamic.Web/Program.cs
+++ MicroBytKonamic.Web/Program.cs
@@ -33,7 +33,7 @@ builder.Services
     .AddControllers();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddProblemDetails().AddHttpClient("github", client => client.BaseAddress = new Uri("https://github.com/Microbyt-Konami/Navidad2023/raw/main/"));
+builder.Services.AddProblemDetails().AddHttpClient("github", client => client.BaseAddress = new Uri("https://github.com/Microbyt-Konami/WebSite/raw/main/"));
 
 var app = builder.Build();
 
